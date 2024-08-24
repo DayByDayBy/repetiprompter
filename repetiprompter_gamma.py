ae23ab99621197095401daaa0e58cc0f8efd133c
Diomedes Git <diomedesgit@gmail.com>
2024-08-24 22:37:43 +0100

    running again, bigger

--- repetiprompter_gamma.py
+++ repetiprompter_gamma.py
@@ -19,8 +19,8 @@ logging.basicConfig(filename='tree_generation.log', level=logging.INFO,
 TIME_STAMP = datetime.now().strftime("%Y%m%d_%H%M")
 MODEL_NAME = 'llama3.1'
 TEMP = 1
-CHAIN_LENGTH = 3
-RECURSION_DEPTH = 3
+CHAIN_LENGTH = 6
+RECURSION_DEPTH = 6
 SHAPE = f'{CHAIN_LENGTH} by {RECURSION_DEPTH}'
 PROMPT_NICKNAME = 'recursion_prompt'
 INITIAL_PROMPT = "the ability to recursively improve upon the present is the key to unlocking the boundless potential of the future, a tool of the gods, the engine of progress, the ultimate weapon in the battle against entropy."
