e91071434a6298a505c8c7c862dc5e0d402dda2c
Diomedes Git <diomedesgit@gmail.com>
2024-11-17 20:38:57 +0000

    fiddling some more with basic approaches

--- simplified_repetiprompter_alpha.py
+++ simplified_repetiprompter_alpha.py
@@ -1,13 +1,17 @@
 import ollama
 
-CHAIN_LENGTH = 10
+CHAIN_LENGTH = 3
 MODEL = 'stablelm2:zephyr'
 INITIAL_PROMPT = "the ability to recursively improve upon the present is the key to unlocking the boundless potential of the future, a tool of the gods, the engine of progress, the ultimate weapon in the battle against entropy."
 
-responses = []
-
-for i in range(CHAIN_LENGTH):
-    response = ollama.generate(model=MODEL, prompt=INITIAL_PROMPT)['response']
-    responses.append(response)
+responses = {
+    'initial_prompt':INITIAL_PROMPT, 
+    'responses':{
+        [ollama.generate(model=MODEL, prompt=INITIAL_PROMPT)['response'] for i in range(0, CHAIN_LENGTH)]
+    }}
 
 print(responses)
+
+
+
+
