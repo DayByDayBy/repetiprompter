4b4ac21543c390eb5f7df51a31f979a01d01377c
Diomedes Git <diomedesgit@gmail.com>
2025-01-07 16:44:08 +0000

    zeph 2by10 output

--- repetiprompter_zeta.py
+++ repetiprompter_zeta.py
@@ -17,7 +17,7 @@ logging.basicConfig(filename='tree_generation.log', level=logging.INFO,
 TIME_STAMP = datetime.now().strftime("%Y%m%d_%H%M")
 MODEL_NAME = 'stablelm2:zephyr'
 CHAIN_LENGTH = 2
-RECURSION_DEPTH = 12
+RECURSION_DEPTH = 10
 BASE_TEMP = 0.5
 MAX_TEMP = 1.0
 SHAPE = f'{CHAIN_LENGTH} by {RECURSION_DEPTH}'
